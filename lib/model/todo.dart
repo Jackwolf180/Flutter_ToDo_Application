056b955032814525cc0486eb529db4c400ef2e83
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-08-05 23:07:34 +0530

    Changed the app Icon

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -10,14 +10,10 @@ class ToDo{
 });
   static List<ToDo>todoList(){
     return[
-      ToDo(id: '01', todoText: "morning aljsdfklajldkfj",isDone: true),
-      ToDo(id: '02', todoText: "morning sdlfhakds",isDone: true),
-      ToDo(id: '03', todoText: "morning aladsfadfcizvcjldkfj",),
-      ToDo(id: '04', todoText: "morning aljsdfklajldkfj",),
-      ToDo(id: '05', todoText: "morning ajewrnemfn cj",),
-      ToDo(id: '06', todoText: "morning aljsdfklajldkfj",),
-      ToDo(id: '07', todoText: "morning aasiodfiasd e kfj",),
-      ToDo(id: '08', todoText: "morning al5sf4asd4fj",),
+      ToDo(id: '01', todoText: "morning exercise",isDone: true),
+      ToDo(id: '02', todoText: "birthday wishes",isDone: true),
+      ToDo(id: '03', todoText: "Holiday Hussel",),
+      ToDo(id: '04', todoText: "Crib Walk :))",),
     ];
   }
 }
